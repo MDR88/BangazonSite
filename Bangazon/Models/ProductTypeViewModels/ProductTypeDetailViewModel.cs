4a8cd49c4f6a29f82bbc1b7e4c9ed32e961936e8
Jewel Ramirez <chimpy4u@yahoo.com>
2018-10-17 11:09:01 -0700

    made detail views

---
+++ Bangazon/Models/ProductTypeViewModels/ProductTypeDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bangazon.Models.ProductTypeViewModels
+{
+    public class ProductTypeDetailViewModel
+    {
+       
+        public string Label { get; set; }
+
+        public virtual ICollection<Product> Products { get; set; }
+
+    }
+}
