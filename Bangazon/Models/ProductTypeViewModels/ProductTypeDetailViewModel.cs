e4764845d7044a3edbfb791a4383f4accddaaaf4
Jewel Ramirez <chimpy4u@yahoo.com>
2018-10-18 09:30:04 -0700

    got my details working!

--- Bangazon/Models/ProductTypeViewModels/ProductTypeDetailViewModel.cs
+++ Bangazon/Models/ProductTypeViewModels/ProductTypeDetailViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,9 +9,9 @@ namespace Bangazon.Models.ProductTypeViewModels
     public class ProductTypeDetailViewModel
     {
        
-        public string Label { get; set; }
+        public  ProductType ProductType { get; set; }
 
-        public virtual ICollection<Product> Products { get; set; }
+
 
     }
 }
