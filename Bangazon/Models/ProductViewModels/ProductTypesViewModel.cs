9e0c324cd874235d1430c2a8062f7ceaaa164285
Emily <ebventurino@gmail.com>
2018-10-18 13:49:12 -0500

    added items to categories

--- Bangazon/Models/ProductViewModels/ProductTypesViewModel.cs
+++ Bangazon/Models/ProductViewModels/ProductTypesViewModel.cs
@@ -7,6 +7,8 @@ namespace Bangazon.Models.ProductViewModels
     public class ProductTypesViewModel
     {
         public List <GroupedProducts> GroupedProducts { get; set; }
+        public List <Product> Products { get; set; }
+        public List <Product> Title { get; set; }
     }
 
 
